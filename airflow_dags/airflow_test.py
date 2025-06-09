cbfa002e45507d8428c309b9c047cf0ca2c685fd
newtypesup <shssyup2@gmail.com>
2025-06-09 23:27:52 +0900

    airflow local test3 250609

--- airflow_dags/airflow_test.py
+++ airflow_dags/airflow_test.py
@@ -1,10 +1,17 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
-with DAG(dag_id="hello_world", start_date=datetime(2025, 1, 1), schedule="@daily", catchup=False) as dag:
+
+KST = timezone(timedelta(hours=9))
+
+with DAG(
+    dag_id="hello_world",
+    start_date=datetime(2025, 6, 8, 20, 50, tzinfo=KST),  # 한국 시간
+    schedule="*/10 * * * *",
+    catchup=False
+) as dag:
     hello = BashOperator(
         task_id="say_hello",
         bash_command="echo Hello from Airflow!"
     )
-#test
