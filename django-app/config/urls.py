b79cd834563cf082034317357735205fc0626d85
newtypesup <shssyup2@gmail.com>
2025-06-15 22:29:36 +0900

    django home test 250615

--- django-app/config/urls.py
+++ django-app/config/urls.py
@@ -1,7 +1,9 @@
 """config URL Configuration"""
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ] 
