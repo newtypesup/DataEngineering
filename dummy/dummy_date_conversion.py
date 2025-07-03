fad1e375c5a64262424b048c33d68ed0f5b1da20
newtypesup <shssyup2@gmail.com>
2025-07-04 00:02:07 +0900

    dummy data update4 250704

--- dummy/dummy_date_conversion.py
+++ dummy/dummy_date_conversion.py
@@ -1,29 +1,24 @@
 import random
 import pandas as pd
 from time import time
-from faker import Faker
 from datetime import datetime, timedelta
 from dummy_utils import db_conn, get_input_table, load_df
-
-fake = Faker()
-Faker.seed(0)
 
 engine = db_conn()
 query = get_input_table()
 df_input = load_df(engine, query)
 
-def _reg_date_conversion(engine, df_input):
+def _reg_date_conversion(df_input):
     start_date = datetime.strptime('2020-01-01', "%Y-%m-%d").date()
     end_date = datetime.now().date()
     df_input['reg_date'] = [(start_date + timedelta(days = random.randint(0, (end_date - start_date).days))).strftime("%Y-%m-%d") for _ in range(len(df_input))]
-
-    with engine.connect() as conn:
-        df_input.to_sql('df_input', conn, if_exists = 'replace', index = False, chunksize = 100)
+    return df_input
 
 if __name__ == "__main__":
   start_time = time()
   try:
-    _reg_date_conversion(engine, df_input)
+    df_input = _reg_date_conversion(df_input)
+    df_input.to_sql('df_input', engine, if_exists = 'replace', index = False, chunksize = 100)
     print(df_input.head())
     print("=" * 50)
     print("success")
--- dummy/dummy_utils.py
+++ dummy/dummy_utils.py
@@ -1,59 +1,56 @@
 # import os
-# import pandas as pd
-# from sqlalchemy import create_engine
+# base_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
+# sys.path.append(base_path)
+import pandas as pd
+from sqlalchemy import create_engine, text
 # from dotenv import load_dotenv
+# base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# dotenv_path = os.path.join(base_path, '.env')
+# load_dotenv(dotenv_path=dotenv_path)
 
-# load_dotenv()
+DB_HOST='localhost'
+DB_PORT=5434
+DB_USER='fastapi'
+DB_PASSWORD=12345
+DB_NAME='fastapi'
+INPUT_TABLE='df_input'
+CCD_TABLE=['df_category','df_content','df_download']
 
-
+def db_conn():
+  username = DB_USER
+  password = DB_PASSWORD
+  host = DB_HOST
+  port = DB_PORT
+  database = DB_NAME
+  engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{database}")
+  print('1')
+  return engine
 # def db_conn():
 #   username = os.getenv("DB_USER")
 #   password = os.getenv("DB_PASSWORD")
 #   host = os.getenv("DB_HOST")
 #   port = os.getenv("DB_PORT")
 #   database = os.getenv("DB_NAME")
-#   return create_engine(
-#       f"postgresql://{username}:{password}@{host}:{port}/{database}")
-
-
-# def get_input_table():
-#   input_table = os.getenv("INPUT_TABLE")
-#   return (f"SELECT * FROM {input_table};")
-
-
-# def load_df(engine, query):
-#   return pd.read_sql_query(query, engine)
-
-import os
-import pandas as pd
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-def db_conn():
-  username = os.getenv("DB_USER")
-  password = os.getenv("DB_PASSWORD")
-  host = os.getenv("DB_HOST")
-  port = os.getenv("DB_PORT")
-  database = os.getenv("DB_NAME")
-  engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{database}")
-  return engine
+#   engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{database}")
+#   print('1')
+#   return engine
 
 
 def get_input_table():
-  input_table = os.getenv("INPUT_TABLE")
-  return (f"SELECT * FROM {input_table};")
+  input_table = INPUT_TABLE
+  query = (f"SELECT * FROM {input_table};")
+  print('2')
+  return query
 
 
 def get_ccd_table():
-  ccd_table = os.getenv("CCD_TABLE")
+  ccd_table = CCD_TABLE
   ccd_table_list = [value.strip() for value in ccd_table.split(",")]
-  ccd = [f"SELECT * FROM {table};" for table in ccd_table_list]
-  return ccd
+  query = [f"SELECT * FROM {table};" for table in ccd_table_list]
+  print("3")
+  return query
 
 
 def load_df(engine, query):
-  return pd.read_sql_query(query, engine)
-
+  print('4')
+  return pd.read_sql_query(text(query), engine)
