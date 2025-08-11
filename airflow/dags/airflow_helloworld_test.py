6002d382e25054f45384feac4a05bc8cbfe6f5f3
newtypesup <shssyup2@gmail.com>
2025-08-11 09:25:25 +0900

    airflow test dags modified 250811

--- airflow/dags/airflow_helloworld_test.py
+++ airflow/dags/airflow_helloworld_test.py
@@ -1,7 +1,7 @@
 import pendulum
+from datetime import datetime, timezone, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator # type: ignore
-from datetime import datetime, timezone, timedelta
 
 KST = pendulum.timezone("Asia/Seoul")
 
@@ -9,7 +9,8 @@ with DAG(
     dag_id="hello_world",
     start_date=pendulum.datetime(2025, 6, 1, 1, 1, tz="Asia/Seoul"),
     schedule="*/10 * * * *",
-    catchup=False
+    catchup=False,
+    tags=['test', 'hello, world!']
 ) as dag:
     hello = BashOperator(
         task_id="say_hello",
