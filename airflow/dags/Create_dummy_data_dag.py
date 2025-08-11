fbe59128205fe7faca43d2ee3a29dca8bc1b9b8d
newtypesup <shssyup2@gmail.com>
2025-08-11 10:03:18 +0900

    airflow dag Create_dummy_data_dag modified 250811

--- airflow/dags/Create_dummy_data_dag.py
+++ airflow/dags/Create_dummy_data_dag.py
@@ -1,23 +1,23 @@
 import pendulum
+from datetime import datetime, timezone
 from airflow import DAG
 from airflow.operators.python import PythonOperator # type: ignore
-from datetime import datetime, timezone
 from scripts.Create_dummy_data import create_dummy
 from scripts.Uploading_dummy_parquet_to_s3 import to_parquet_and_upload
-from utils.Slack_alert import slack_alert, slack_success_alert
+from utils.Slack_alert import slack_fail_alert, slack_success_alert
 
 KST = pendulum.timezone("Asia/Seoul")
 
 default_args = {
     'owner': 'airflow',
-    'on_failure_callback': slack_alert,
+    'on_failure_callback': slack_fail_alert,
 }
 with DAG(
-    dag_id="Create_dummy_data_dag",
+    dag_id="Create_dummy_data",
     default_args=default_args,
     schedule_interval="* */2 * * *", # 매 2시간마다
-    start_date=datetime(2025, 7, 1, 0, 0, tzinfo=KST),
-    end_date=datetime(2025, 7, 31, 0, 0, tzinfo=KST),
+    start_date=datetime(2025, 5, 1, 0, 0, tzinfo=KST),
+    # end_date=datetime(2025, 8, 1, 0, 0, tzinfo=KST),
     catchup=False,
     tags=['create', 'dummy', 'upload', 'parquet', 's3']) as dag:
     create_dummy_data = PythonOperator(
